20b698790cbf55415617a87e9708893e14408074
khaingmyozaw <kmyozaw.dev@gmail.com>
2025-08-06 23:49:42 +0630

    api installation

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return response()->json([
+            'message' => 'Hey! This is login',
+            'status' => 200
+        ], 200);
+    }
+}
